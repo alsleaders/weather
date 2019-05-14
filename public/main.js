dde25fb3d6513d5ac3843e0414094a395466e087
Amanda Leaders <alsleaders@gmail.com>
2019-05-14 16:44:11 -0400

    worked, and now trying to get radio buttons working

--- public/main.js
+++ public/main.js
@@ -2,21 +2,52 @@ const main = () => {
   // DONE set up input
   // DONE make button work
 }
+// let cityQuery = document.querySelector('.location-search').value
+// let cityZip =
+// let cityName = cityZip || cityText
+
+const getUnitsForRequest = () => {
+  // get the current value of the radios
+  // default
+  document.getElementById('.kelvin').checked = true
+  return ''
+  // celsius
+  document.getElementById('.celsius').checked = true
+  return 'units=metric'
+  // fahrenheit
+  document.getElementById('.fahrenheit').checked = true
+  return 'units=imperial'
+}
 
 const searchWeatherFromApi = () => {
   // document.querySelector('.search').disabled = true
-  //get city name
+  // get city name
   const cityName = document.querySelector('.location-search').value
   console.log(cityName)
+  // set up fetch
   fetch(
     'https://api.openweathermap.org/data/2.5/weather?q=' +
       cityName +
-      '&appid=017ef3e4bf1e11d8cf9397e2e73fd25a'
-  ).then(response => {
-    console.log('the fetch worked')
-  })
-  // set up fetch
-  // then display
+      '&appid=017ef3e4bf1e11d8cf9397e2e73fd25a' +
+      getUnitsForRequest()
+  )
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    // then display
+    .then(city => {
+      document.querySelector('.city-name').textContent = city.name
+      console.log({ city })
+      console.log(city.weather)
+      console.log(city.weather[0])
+      console.log(city.weather[0].description)
+      document.querySelector('.display-text').textContent =
+        city.weather[0].description
+      document.querySelector('.display-temp').textContent = city.main.temp
+      document.querySelector('.display-wind').textContent =
+        city.wind.speed + ' in m/s'
+    })
   // css stuff for each possible display
 }
 
@@ -24,3 +55,6 @@ document.addEventListener('DOMContentLoaded', main)
 document
   .querySelector('.search')
   .addEventListener('click', searchWeatherFromApi)
+document.querySelectorAll('input[type=radio]').forEach(radio => {
+  radio.addEventListener('click', searchWeatherFromApi)
+})
