e62b6a3f0877e24011584ef9500e587c789e0e3a
Amanda Leaders <alsleaders@gmail.com>
2019-05-14 14:12:11 -0400

    fetch works, work on display

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  // DONE set up input
+  // DONE make button work
+}
+
+const searchWeatherFromApi = () => {
+  // document.querySelector('.search').disabled = true
+  //get city name
+  const cityName = document.querySelector('.location-search').value
+  console.log(cityName)
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      cityName +
+      '&appid=017ef3e4bf1e11d8cf9397e2e73fd25a'
+  ).then(response => {
+    console.log('the fetch worked')
+  })
+  // set up fetch
+  // then display
+  // css stuff for each possible display
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document
+  .querySelector('.search')
+  .addEventListener('click', searchWeatherFromApi)
